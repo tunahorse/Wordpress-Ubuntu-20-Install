f7ac8fb10abfd06e8c126c67922fef87c6b604da
myusername <fabiananguiano@gmai.com>
2021-05-07 16:35:37 -0500

    lets goo

---
+++ README.md
@@ -0,0 +1,20 @@
+# Wordpress_Ubuntu_20_Install
+
+A bash script that installs WordPress with the LAMP stack with one click.  
+## Installation
+
+```git
+Get clone 
+```
+
+
+## Usage
+
+```bash
+sh wordpress_install.sh
+```
+
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ wordpress_install.sh
@@ -0,0 +1,111 @@
+#/bin/sh
+
+install_dir="/var/www/html"
+# Begin Creating  WP Database Credenitals
+db_name="wp`date +%s`"
+db_user=$db_name
+db_password=`date |md5sum |cut -c '1-10'`
+sleep 1
+mysqlrootpass=`date |md5sum |cut -c '1-10'`
+sleep 1
+# End Creating  WP Database Credenitals
+
+#### Begin Install Packages for https and mysql
+apt-get update -y 
+apt -y install apache2
+apt -y install mysql-server
+apt -y install lynx
+apt-get install php-mysql -y
+#### End Install Packages for https and mysql
+
+
+
+#### Begin Start Web Server 
+rm /var/www/html/index.html
+systemctl enable apache2
+systemctl start apache2
+#### End Start Web Server 
+
+
+#### Start mysql and set password
+
+systemctl enable mysql
+systemctl start mysql
+
+/usr/bin/mysql -e "USE mysql;"
+/usr/bin/mysql -e "UPDATE user SET Password=PASSWORD($mysqlrootpass) WHERE user='root';"
+/usr/bin/mysql -e "FLUSH PRIVILEGES;"
+touch /root/.my.cnf
+chmod 640 /root/.my.cnf
+echo "[client]">>/root/.my.cnf
+echo "user=root">>/root/.my.cnf
+echo "password="$mysqlrootpass>>/root/.my.cnf
+#### End mysql and set password
+
+
+#### Begin Install PHP
+apt -y install php
+sed -i '0,/AllowOverride\ None/! {0,/AllowOverride\ None/ s/AllowOverride\ None/AllowOverride\ All/}' /etc/apache2/apache2.conf #Allow htaccess usage
+systemctl restart apache2
+#### End Install PHP
+
+
+#### Begin Download and extract latest WordPress Package
+if test -f /tmp/latest.tar.gz
+then
+echo "WP is already downloaded."
+else
+echo "Downloading WordPress"
+cd /tmp/ && wget "http://wordpress.org/latest.tar.gz";
+fi
+
+/bin/tar -C $install_dir -zxf /tmp/latest.tar.gz --strip-components=1
+chown www-data: $install_dir -R
+#### End Download and extract latest WordPress Package
+
+#### Begin writing to WP-config and set DB credentials
+/bin/mv $install_dir/wp-config-sample.php $install_dir/wp-config.php
+
+/bin/sed -i "s/database_name_here/$db_name/g" $install_dir/wp-config.php
+/bin/sed -i "s/username_here/$db_user/g" $install_dir/wp-config.php
+/bin/sed -i "s/password_here/$db_password/g" $install_dir/wp-config.php
+
+cat << EOF >> $install_dir/wp-config.php
+define('FS_METHOD', 'direct');
+EOF
+
+cat << EOF >> $install_dir/.htaccess
+# BEGIN WordPress
+<IfModule mod_rewrite.c>
+RewriteEngine On
+RewriteBase /
+RewriteRule ^index.php$ – [L]
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteRule . /index.php [L]
+</IfModule>
+# END WordPress
+EOF
+
+chown www-data: $install_dir -R
+
+#### End Create WP-config and set DB credentials
+
+
+##### Begin WP Salts
+grep -A50 'table_prefix' $install_dir/wp-config.php > /tmp/wp-tmp-config
+/bin/sed -i '/**#@/,/$p/d' $install_dir/wp-config.php
+/usr/bin/lynx --dump -width 200 https://api.wordpress.org/secret-key/1.1/salt/ >> $install_dir/wp-config.php
+/bin/cat /tmp/wp-tmp-config >> $install_dir/wp-config.php && rm /tmp/wp-tmp-config -f
+/usr/bin/mysql -u root -e "CREATE DATABASE $db_name"
+/usr/bin/mysql -u root -e "CREATE USER '$db_name'@'localhost' IDENTIFIED WITH mysql_native_password BY '$db_password';"
+/usr/bin/mysql -u root -e "GRANT ALL PRIVILEGES ON $db_name.* TO '$db_user'@'localhost';"
+##### End WP Salts
+
+###### Begin Display generated passwords to log file.
+echo "Database Name: " $db_name
+echo "Database User: " $db_user
+echo "Database Password: " $db_password
+echo "Mysql root password: " $mysqlrootpass
+###### End Display generated passwords to log file.
+
